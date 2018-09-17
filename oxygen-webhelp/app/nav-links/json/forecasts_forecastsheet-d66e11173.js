427b193bb96c755427ddb9b94ca577ca9cc39a0d
Ben Welman <bwelman@gmail.com>
2018-09-18 00:04:40 +0200

    LearnExcel vs 1.35

---
+++ oxygen-webhelp/app/nav-links/json/forecasts_forecastsheet-d66e11173.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Options forecast sheet","shortdesc":"\n                  <p class=\"shortdesc\"></p>\n               ","href":"forecasts\/forecasts_forecastsheet_options.html","attributes": {"data-id":"forecasts_forecastsheet_options",},"menu": {"hasChildren":false,},"tocID":"forecasts_forecastsheet_options-d66e11190","topics":[]}]});
---
+++ webhelp/oxygen-webhelp/app/nav-links/json/tocId-d66e10565.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Exercise: data001 - Import simple csv file","shortdesc":"\n                  <p class=\"shortdesc\">Level: 2</p>\n               ","href":"import\/exercises\/ex_data001.html","attributes": {"data-id":"ex_data001",},"menu": {"hasChildren":false,},"tocID":"ex_data001-d66e10566","topics":[]}]});
