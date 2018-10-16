9b21d31630df469dd100c5236e7ef37c055d327e
Ben Welman <bwelman@gmail.com>
2018-10-16 13:22:03 +0200

    Start new chapter tidy data

---
+++ oxygen-webhelp/app/nav-links/json/tidydata-d66e11506.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Task: Unpivot data","shortdesc":"\n               <p class=\"shortdesc\">Unpivot data that is in crosstab\/pivot format.</p>\n            ","href":"tidydata\/pivots_unpivoting.html","attributes": {"data-id":"pivots_unpivoting",},"menu": {"hasChildren":false,},"tocID":"pivots_unpivoting-d66e11523","topics":[]}]});
