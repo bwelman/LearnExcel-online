84609e4eca1304bcc9a9ccdb489803afa4f3df7a
Ben Welman <bwelman@gmail.com>
2019-08-29 11:30:43 +0200

    Changed title and colophon.

---
+++ oxygen-webhelp/app/nav-links/json/tidydata-d62e11506.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Task: Unpivot data","shortdesc":"<p class=\"shortdesc\">Unpivot data that is in crosstab\/pivot format.</p>","href":"tidydata\/pivots_unpivoting.html","attributes": {"data-id":"pivots_unpivoting",},"menu": {"hasChildren":false,},"tocID":"pivots_unpivoting-d62e11523","topics":[]}]});
