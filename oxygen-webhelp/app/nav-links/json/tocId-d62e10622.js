864441bf63cabd8a245f07d3ea8dd54f32b33d8d
Ben Welman <bwelman@gmail.com>
2019-09-20 10:22:28 +0200

    Added exercises for chapter Arrays. Added answers for some exercises.

---
+++ oxygen-webhelp/app/nav-links/json/tocId-d62e10622.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Exercise: data001 - Import simple csv file","shortdesc":"<p class=\"shortdesc\">Level: 2</p>","href":"import\/exercises\/ex_data001.html","attributes": {"data-id":"ex_data001",},"menu": {"hasChildren":false,},"tocID":"ex_data001-d62e10623","topics":[]}]});
