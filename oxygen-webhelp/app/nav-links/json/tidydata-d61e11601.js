c172770c1484272f60139dd3b68abbbef7085b56
Ben Welman <bwelman@gmail.com>
2020-06-14 20:30:31 +0200

    vs 1.45: added chapter query options, correction of a lot of spelling errors.

---
+++ oxygen-webhelp/app/nav-links/json/tidydata-d61e11601.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Task: Unpivot data","shortdesc":"<p class=\"shortdesc\">Unpivot data that is in the crosstab\/pivot format.</p>","href":"tidydata\/pivots_unpivoting.html","attributes": {"data-id":"pivots_unpivoting",},"menu": {"hasChildren":false,},"tocID":"pivots_unpivoting-d61e11618","topics":[]}]});
---
+++ oxygen-webhelp/app/nav-links/json/tocId-d61e10660.js
@@ -0,0 +1,1 @@
+define({"topics" : [{"title":"Exercise: data001 - Import simple csv file","shortdesc":"<p class=\"shortdesc\">Level: 2</p>","href":"import\/exercises\/ex_data001.html","attributes": {"data-id":"ex_data001",},"menu": {"hasChildren":false,},"tocID":"ex_data001-d61e10661","topics":[]}]});
